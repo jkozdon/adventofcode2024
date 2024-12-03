513a76b16d156ac25de83b67defbd6e22711edc4
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-02 16:17:36 -0800

    Day02: part a
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day02.cpp
+++ src/day02.cpp
@@ -1,10 +1,40 @@
+#include <ranges>
+#include <cassert>
+
 #include "common.hpp"
 #include "fmt/core.h"
 
 namespace day02
 {
 
-void part1(const std::string &input, const bool test) {}
+void part1(const std::string &input, const bool test)
+{
+  auto lines = input | std::views::split('\n');
+  int res = 0;
+  for (auto line : lines) {
+    if (line.empty()) {
+      break;
+    }
+    int next{};
+    int last{};
+    std::istringstream ss(std::ranges::to<std::string>(line));
+    ss >> last;
+    ss >> next;
+    bool increasing = next > last;
+    bool valid = true;
+    do {
+      auto delta = std::abs(last - next);
+      valid =
+          0 < delta && delta < 4 && (increasing ? next > last : next < last);
+      last = next;
+    } while (valid && ss >> next);
+    res += valid;
+  }
+  fmt::println("  Part a: {}", res);
+  if (test) {
+    assert(res == 2);
+  }
+}
 
 void part2(const std::string &input, const bool test) {}
 
