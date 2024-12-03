ad85ccf70bb1c4a2d42ee801b66393be0274c971
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-03 10:20:01 -0800

    Day03: part a
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day03.cpp
+++ src/day03.cpp
@@ -1,10 +1,27 @@
 #include "common.hpp"
 #include "fmt/core.h"
 
+#include <regex>
+#include <string>
+#include <cassert>
+
 namespace day03
 {
 
-void part1(const std::string &input, const bool test) {}
+void part1(const std::string &input, const bool test)
+{
+  std::regex regex(R"(mul\(([0-9]+),([0-9]+)\))");
+  auto match = std::sregex_iterator(input.begin(), input.end(), regex);
+  int res = 0;
+  for (; match != std::sregex_iterator(); ++match) {
+    res += std::atoi((*match)[1].str().c_str()) *
+           std::atoi((*match)[2].str().c_str());
+  }
+  fmt::println("  Part a: {}", res);
+  if (test) {
+    assert(res == 161);
+  }
+}
 
 void part2(const std::string &input, const bool test) {}
 
