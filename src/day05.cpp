61191d8716035b893ac340ee510dcf463946a02a
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-05 18:00:52 -0800

    day05: part a
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day05.cpp
+++ src/day05.cpp
@@ -1,17 +1,57 @@
+#include <ranges>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+
 #include "common.hpp"
 #include "fmt/core.h"
-
-#include <cassert>
 
 namespace day05
 {
 
 void part1(const std::string &input, const bool test)
 {
+
+  std::unordered_map<int, std::unordered_set<int>> links;
+
+  auto lines = input | std::views::split('\n');
+  auto next_line = lines.begin();
+  for (; !(*next_line).empty(); ++next_line) {
+    auto line = *next_line;
+    std::istringstream ss(std::ranges::to<std::string>(line));
+    int a, b;
+    char sep;
+    ss >> a >> sep >> b;
+    links[a].insert(b);
+  }
+  for (; (*next_line).empty(); ++next_line)
+    ;
   int res = 0;
+  for (; !(*next_line).empty(); ++next_line) {
+    auto line = *next_line;
+    std::istringstream ss(std::ranges::to<std::string>(line));
+    int a;
+    char sep;
+    std::unordered_set<int> seen;
+    bool valid = true;
+    int midpnt = (line.size() / 3) / 2;
+    int mid{};
+    do {
+      ss >> a >> sep;
+      for (auto b : seen) {
+        valid = valid & !(links[a].contains(b));
+      }
+      seen.insert(a);
+      if (midpnt == 0)
+        mid = a;
+      --midpnt;
+    } while (ss && valid);
+    res += valid ? mid : 0;
+  }
+
   fmt::print("  Part a: {}\n", res);
   if (test) {
-    assert(res == 0);
+    assert(res == 143);
   }
 }
 
