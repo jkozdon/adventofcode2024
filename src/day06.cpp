548e11a6da7955224dbcc1475b31c99914820634
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-07 16:37:42 -0800

    day06: part a
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day06.cpp
+++ src/day06.cpp
@@ -1,17 +1,85 @@
+#include <ranges>
+#include <vector>
+#include <cassert>
+
 #include "common.hpp"
 #include "fmt/core.h"
-
-#include <cassert>
 
 namespace day06
 {
 
+enum direction { up=1, down=2, right=4, left=8 };
+
 void part1(const std::string &input, const bool test)
 {
+
+  auto lines = input | std::views::split('\n') |
+               std::ranges::to<std::vector<std::string>>();
+  int gx = 0;
+  int gy = 0;
+  [&] {
+    for (auto line : lines) {
+      gx = 0;
+      for (auto c : line) {
+        if (c == '^') {
+          lines[gy][gx] = '.';
+          return;
+        }
+        ++gx;
+      }
+      ++gy;
+    }
+  }();
+
+  int Nx = lines[0].size();
+  int Ny = lines.size();
+
+  direction dir = up;
+
   int res = 0;
+  while (true) {
+    if (lines[gy][gx] == '.') {
+      ++res;
+      lines[gy][gx] = 'X';
+    }
+    if (dir == up) {
+      if (gy == 0)
+        break;
+      if (lines[gy - 1][gx] != '#') {
+        gy -= 1;
+      } else {
+        dir = right;
+      }
+    } else if (dir == right) {
+      if (gx == Nx - 1)
+        break;
+      if (lines[gy][gx + 1] != '#') {
+        gx += 1;
+      } else {
+        dir = down;
+      }
+    } else if (dir == down) {
+      if (gy == Ny - 1)
+        break;
+      if (lines[gy + 1][gx] != '#') {
+        gy += 1;
+      } else {
+        dir = left;
+      }
+    } else {
+      if (gx == 0)
+        break;
+      if (lines[gy][gx - 1] != '#') {
+        gx -= 1;
+      } else {
+        dir = up;
+      }
+    }
+  }
+
   fmt::print("  Part a: {}\n", res);
   if (test) {
-    assert(res == 0);
+    assert(res == 41);
   }
 }
 
