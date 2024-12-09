6260c4f0c26ea57243cd694c08d817f4c6f4579d
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-09 12:36:11 -0800

    day09: stub
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day09.cpp
+++ src/day09.cpp
@@ -1,23 +1,23 @@
+#include <cassert>
+
 #include "common.hpp"
 #include "fmt/core.h"
-
-#include <cassert>
 
 namespace day09
 {
 
 void part1(const std::string &input, const bool test)
 {
-  int res = 0;
+  uint64_t res = 0;
   fmt::print("  Part a: {}\n", res);
   if (test) {
-    assert(res == 0);
+    assert(res == 1928L);
   }
 }
 
 void part2(const std::string &input, const bool test)
 {
-  int res = 0;
+  uint64_t res = 0;
   fmt::print("  Part a: {}\n", res);
   if (test) {
     assert(res == 0);
@@ -26,7 +26,6 @@ void part2(const std::string &input, const bool test)
 
 void run(const std::string &base_path, const bool test)
 {
-  return;
   auto input = read_input(base_path, 9, test);
   fmt::println("Day 09:");
   part1(input, test);
