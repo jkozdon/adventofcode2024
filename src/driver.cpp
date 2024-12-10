d4acda70b910cc9513a1158929980f0868f6fac1
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-09 21:30:05 -0800

    driver: add day selector
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/driver.cpp
+++ src/driver.cpp
@@ -36,29 +36,54 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  day01::run(base_path, test);
-  day02::run(base_path, test);
-  day03::run(base_path, test);
-  day04::run(base_path, test);
-  day05::run(base_path, test);
-  day06::run(base_path, test);
-  day07::run(base_path, test);
-  day08::run(base_path, test);
-  day09::run(base_path, test);
-  day10::run(base_path, test);
-  day11::run(base_path, test);
-  day12::run(base_path, test);
-  day13::run(base_path, test);
-  day14::run(base_path, test);
-  day15::run(base_path, test);
-  day16::run(base_path, test);
-  day17::run(base_path, test);
-  day18::run(base_path, test);
-  day19::run(base_path, test);
-  day20::run(base_path, test);
-  day21::run(base_path, test);
-  day22::run(base_path, test);
-  day23::run(base_path, test);
-  day24::run(base_path, test);
-  day25::run(base_path, test);
+  if (day == 0 || day == 1)
+    day01::run(base_path, test);
+  if (day == 0 || day == 2)
+    day02::run(base_path, test);
+  if (day == 0 || day == 3)
+    day03::run(base_path, test);
+  if (day == 0 || day == 4)
+    day04::run(base_path, test);
+  if (day == 0 || day == 5)
+    day05::run(base_path, test);
+  if (day == 0 || day == 6)
+    day06::run(base_path, test);
+  if (day == 0 || day == 7)
+    day07::run(base_path, test);
+  if (day == 0 || day == 8)
+    day08::run(base_path, test);
+  if (day == 0 || day == 9)
+    day09::run(base_path, test);
+  if (day == 0 || day == 10)
+    day10::run(base_path, test);
+  if (day == 0 || day == 11)
+    day11::run(base_path, test);
+  if (day == 0 || day == 12)
+    day12::run(base_path, test);
+  if (day == 0 || day == 13)
+    day13::run(base_path, test);
+  if (day == 0 || day == 14)
+    day14::run(base_path, test);
+  if (day == 0 || day == 15)
+    day15::run(base_path, test);
+  if (day == 0 || day == 16)
+    day16::run(base_path, test);
+  if (day == 0 || day == 17)
+    day17::run(base_path, test);
+  if (day == 0 || day == 18)
+    day18::run(base_path, test);
+  if (day == 0 || day == 19)
+    day19::run(base_path, test);
+  if (day == 0 || day == 20)
+    day20::run(base_path, test);
+  if (day == 0 || day == 21)
+    day21::run(base_path, test);
+  if (day == 0 || day == 22)
+    day22::run(base_path, test);
+  if (day == 0 || day == 23)
+    day23::run(base_path, test);
+  if (day == 0 || day == 24)
+    day24::run(base_path, test);
+  if (day == 0 || day == 25)
+    day25::run(base_path, test);
 }
