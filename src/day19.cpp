d40798c05be55472df9518b2c774ab424c0baa0d
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-19 19:44:48 -0800

    day19: part b
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day19.cpp
+++ src/day19.cpp
@@ -2,6 +2,7 @@
 #include <regex>
 #include <stack>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <cassert>
 
@@ -62,10 +63,42 @@ void part1(const std::string &input, const bool test)
 
 void part2(const std::string &input, const bool test)
 {
-  int res = 0;
+  std::unordered_set<std::string> towels;
+  std::regex regex(R"(([a-z]+)([, ]?))");
+  auto newline_pos = input.find('\n');
+  std::string first_line = input.substr(0, newline_pos);
+  auto match =
+      std::sregex_iterator(first_line.begin(), first_line.end(), regex);
+
+  for (auto it = match; it != std::sregex_iterator(); ++it) {
+    towels.insert((*it)[1]);
+  }
+  uint64_t res = 0;
+
+  auto lines =
+      input | std::ranges::views::split('\n') | std::ranges::views::drop(2);
+  for (auto vline : lines) {
+    if (vline.empty())
+      continue;
+    auto line = std::ranges::to<std::string>(vline);
+    std::unordered_map<int, uint64_t> found;
+    found[0] = 1;
+    for (int i = 1; i <= line.size(); ++i) {
+      for (auto t : towels) {
+        auto l = t.size();
+        if (l <= i) {
+          if (line.substr(i - l, l) == t) {
+            found[i] += found[i - l];
+          }
+        }
+      }
+    }
+    res += found[line.size()];
+  }
+
   fmt::print("  Part b: {}\n", res);
   if (test) {
-    assert(res == 0);
+    assert(res == 16);
   }
 }
 
