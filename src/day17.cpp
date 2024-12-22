6eebe3f78120ee28bfe5cc9033285ae6ed61ba1f
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-22 07:03:15 -0800

    day17: part b
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day17.cpp
+++ src/day17.cpp
@@ -6,6 +6,7 @@
 #include "fmt/core.h"
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 namespace day17
 {
@@ -88,10 +89,91 @@ void part1(const std::string &input, const bool test)
 
 void part2(const std::string &input, const bool test)
 {
-  int res = 0;
-  fmt::print("  Part b: {}\n", res);
   if (test) {
-    assert(res == 0);
+    return;
+  }
+  auto prog_ind = input.find(",") - 1;
+  std::vector<int8_t> prog;
+  for (auto c : input.substr(prog_ind)) {
+    if ('0' <= c && c <= '9') {
+      prog.push_back(c - '0');
+    }
+  }
+  std::unordered_set<uint64_t> regA;
+  regA.insert(0);
+  for (auto pd = prog.rbegin(); pd != prog.rend(); ++pd) {
+    auto cur_regA = regA;
+    const auto d = *pd;
+    if (regA.empty()) {
+      fmt::println("regA is empty!");
+      break;
+    }
+    regA.clear();
+    for (auto Ain : cur_regA) {
+      Ain = Ain << 3;
+      for (uint64_t delta = 0; delta < 8; ++delta) {
+        uint64_t A = Ain + delta;
+        uint64_t B = 0;
+        uint64_t C = 0;
+        int pc = 0;
+        auto combo = [&](int8_t op) {
+          if (op == 4) {
+            return A;
+          } else if (op == 5) {
+            return B;
+          } else if (op == 6) {
+            return C;
+          } else if (op == 7) {
+            fmt::println("Illegal op code 7");
+          }
+          return (uint64_t)op;
+        };
+        std::string out;
+        while (pc < prog.size()) {
+          auto in = prog[pc];
+          auto op = prog[pc + 1];
+          pc += 2;
+          if (in == 0 || in == 6 || in == 7) { // adv,bdv,cdv
+            auto num = A;
+            auto den = 1 << combo(op);
+            auto res = num / den;
+            if (in == 0) {
+              A = res;
+            } else if (in == 6) {
+              B = res;
+            } else {
+              C = res;
+            }
+          } else if (in == 1) { // bxl
+            B = B ^ op;
+          } else if (in == 2) { // bst
+            B = combo(op) % 8;
+          } else if (in == 3) { // jnz
+            if (A != 0) {
+              pc = op;
+            }
+          } else if (in == 4) { // bxc
+            B = B ^ C;
+          } else if (in == 5) { // out
+            if (d == combo(op) % 8) {
+              regA.insert(Ain + delta);
+            }
+            break;
+          } else if (in > 8 || in < 0) {
+            fmt::println("Illegal instruction!");
+            return;
+          }
+        }
+      }
+    }
+  }
+  auto A = *regA.begin();
+  for (auto At : regA) {
+    A = A > At ? At : A;
+  }
+  fmt::print("  Part b: {}\n", A);
+  if (test) {
+    // assert(out == "4,6,3,5,6,3,5,2,1,0");
   }
 }
 
