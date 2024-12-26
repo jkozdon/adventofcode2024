8c5fe8271cffa684c04222bdc56dca8f0c5758f7
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-26 12:32:02 -0800

    day22: part a
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day22.cpp
+++ src/day22.cpp
@@ -1,17 +1,34 @@
 #include <cassert>
+#include <cstdint>
 
 #include "common.hpp"
 #include "fmt/core.h"
+#include <ranges>
+#include <string>
 
 namespace day22
 {
 
 void part1(const std::string &input, const bool test)
 {
-  int res = 0;
+  auto mix_prune = [](uint64_t a, uint64_t b) { return (a ^ b) % 16777216; };
+  auto lines = input | std::ranges::views::split('\n');
+  uint64_t res{0};
+  for (auto line : lines) {
+    if (line.empty()) {
+      continue;
+    }
+    uint64_t num = std::atol(std::ranges::to<std::string>(line).c_str());
+    for (int i = 0; i < 2000; ++i) {
+      num = mix_prune(num, num * 64);
+      num = mix_prune(num, num / 32);
+      num = mix_prune(num, num * 2048);
+    }
+    res += num;
+  }
   fmt::print("  Part a: {}\n", res);
   if (test) {
-    assert(res == 0);
+    assert(res == 37327623L);
   }
 }
 
@@ -26,7 +43,6 @@ void part2(const std::string &input, const bool test)
 
 void run(const std::string &base_path, const bool test)
 {
-  return;
   auto input = read_input(base_path, 22, test);
   fmt::println("Day 22:");
   part1(input, test);
