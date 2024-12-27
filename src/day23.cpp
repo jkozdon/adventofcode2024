6609a4fb19e7fe27cd745a96244157c4797fe2c4
Jeremy E Kozdon <jeremy@kozdon.net>
2024-12-27 09:49:54 -0800

    day23: part a
    Signed-off-by: Jeremy E Kozdon <jeremy@kozdon.net>

--- src/day23.cpp
+++ src/day23.cpp
@@ -1,4 +1,10 @@
+#include <ranges>
+#include <string>
+#include <sys/types.h>
+#include <unordered_map>
+#include <unordered_set>
 #include <cassert>
+#include <cstdint>
 
 #include "common.hpp"
 #include "fmt/core.h"
@@ -8,10 +14,67 @@ namespace day23
 
 void part1(const std::string &input, const bool test)
 {
-  int res = 0;
+  auto lines = input | std::ranges::views::split('\n');
+  std::unordered_map<std::string, uint64_t> name_to_ind;
+  std::unordered_map<uint64_t, std::string> ind_to_name;
+  std::unordered_map<uint64_t, std::unordered_set<uint64_t>> edges;
+  std::unordered_set<uint64_t> tnodes;
+  uint64_t next_ind{0};
+  for (auto line : lines) {
+    if (line.empty()) {
+      continue;
+    }
+    std::string node_a = {line[0], line[1]};
+    if (!name_to_ind.contains(node_a)) {
+      ind_to_name[next_ind] = node_a;
+      name_to_ind[node_a] = next_ind++;
+    }
+    auto ind_a = name_to_ind[node_a];
+
+    std::string node_b = {line[3], line[4]};
+    if (!name_to_ind.contains(node_b)) {
+      ind_to_name[next_ind] = node_b;
+      name_to_ind[node_b] = next_ind++;
+    }
+    auto ind_b = name_to_ind[node_b];
+    edges[ind_a].insert(ind_b);
+    edges[ind_b].insert(ind_a);
+    if (node_a[0] == 't') {
+      tnodes.insert(ind_a);
+    }
+    if (node_b[0] == 't') {
+      tnodes.insert(ind_b);
+    }
+  }
+
+  // count triangles
+  std::unordered_set<uint64_t> seen;
+  uint64_t res = 0;
+  for (auto n : tnodes) {
+    auto end = edges[n].end();
+    seen.insert(n);
+    for (auto nbrs = edges[n].begin(); nbrs != end; ++nbrs) {
+      auto n_nbrs = nbrs;
+      n_nbrs++;
+      auto i = *nbrs;
+      auto &i_edges = edges[i];
+      if (seen.contains(i)) {
+        continue;
+      }
+      for (; n_nbrs != end; ++n_nbrs) {
+        auto j = *n_nbrs;
+        if (seen.contains(j)) {
+          continue;
+        }
+        if (i_edges.contains(j)) {
+          ++res;
+        }
+      }
+    }
+  }
   fmt::print("  Part a: {}\n", res);
   if (test) {
-    assert(res == 0);
+    assert(res == 7);
   }
 }
 
